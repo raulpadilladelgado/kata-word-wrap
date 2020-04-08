8c44bfdace89f08886d3d59759ef5bb90ec202c4
Raúl Padilla Delgado <rpd27@outlook.com>
2020-04-08 17:52:41 +0100

    only recursion

--- src/test/java/WordWrapShould.java
+++ src/test/java/WordWrapShould.java
@@ -12,90 +12,54 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class WordWrapShould {
     @Test
-    public void result_must_be_empty() {
+    public void consider_as_empty_texts() {
         assertThat(wordWrap("", 2)).isEqualTo("");
         assertThat(wordWrap(null, 2)).isEqualTo("");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void result_must_be_Ilegal_Argument_Exception() {
+    public void not_accept_integers_smaller_than_one() {
         wordWrap("hola", 0);
     }
 
     @Test
-    public void text_must_be_wrap() {
+    public void put_or_replace_wrap_lines() {
         assertThat(wordWrap("hola", 2)).isEqualTo("ho\nla");
         assertThat(wordWrap("hola", 3)).isEqualTo("hol\na");
         assertThat(wordWrap("holaqu", 2)).isEqualTo("ho\nla\nqu");
         assertThat(wordWrap("holaquetalestas", 2)).isEqualTo("ho\nla\nqu\net\nal\nes\nta\ns");
 
-        //assertThat(wordWrapRecursividad("hola mundo", 6)).isEqualTo("hola\nmundo");
-        // assertThat(wordWrapRecursividad("hola mundodffdfdfdfdf", 6)).isEqualTo("hola\nmundodf\nfdfdfd\nfdf");
-        // assertThat(wordWrapRecursividad("hello world", 7)).isEqualTo("hello\nworld");
-        // assertThat(wordWrapRecursividad("a lot of words", 10)).isEqualTo("a lot of\nwords");
-        // assertThat(wordWrapRecursividad("a lot of words for a single line", 10)).isEqualTo("a lot of\nwords for\na single\nline");
+        assertThat(wordWrap("hola mundo", 6)).isEqualTo("hola\nmundo");
+        assertThat(wordWrap("hola mundodffdfdfdfdf",6)).isEqualTo("hola\nmundodf\nfdfdfd\nfdf");
+        assertThat(wordWrap("a lot of words", 10)).isEqualTo("a lot of\nwords");
+        assertThat(wordWrap("a lot of words for a single line",10)).isEqualTo("a lot of\nwords for\na single\nline");
     }
 
-    //---------------------------------------------------------------------
-    //CON RECURSIVIDAD
     private String wordWrap(String text, int columnWidth) {
         if (columnWidth < 1) {
             throw new IllegalArgumentException("Column width must be one or more");
         }
-        if (text == null||text.isEmpty()) {
+        if (text == null || text.isEmpty()) {
             return "";
         }
 
-        if (text.length()<=columnWidth){return text;}
-        String result = text.substring(0,columnWidth)+"\n";
+        if (text.length() <= columnWidth) {
+            return text;
+        }
+        String result = text.substring(0, columnWidth);
+        int lastWhiteSpaceIndex = result.lastIndexOf(" ");
         String remainder = text.substring(columnWidth);
-        //PRE-RECURSIVIDAD
-        // if (remainder.length()>columnWidth){
-        //     result += remainder.substring(0,columnWidth)+"\n";
-        //     result += remainder.substring(columnWidth);
-        //     return result;
-        // }
-        return result+wordWrap(remainder, columnWidth);
-    }
-//--------------------------------------------------------------------------
-    //SIN RECURSIVIDAD
-    /*private String wordWrapRecursividad(String text, int columnWidth) {
-        if (columnWidth < 1) {
-            throw new IllegalArgumentException("Column width must be one or more");
+        if (lastWhiteSpaceIndex != -1) {
+            result = changeWhiteSpaceToLineWrap(result, lastWhiteSpaceIndex);
+            return result + wordWrap(remainder, columnWidth);
         }
-        if (text == null) {
-            return "";
-        }
-
-        boolean textIsGreaterThanColumnWidth = text.length() > columnWidth;
-        if (textIsGreaterThanColumnWidth) {
-            String result = "";
-            for (int currentPosition = 0; currentPosition < text.length(); currentPosition += columnWidth) {
-                boolean currentPositionIsLessThanRemainderText = currentPosition < text.length() - columnWidth;
-                if (currentPositionIsLessThanRemainderText) {
-                    result += wrapText(text, columnWidth, currentPosition);
-                } else {
-                    String remainder = text.substring(currentPosition); 
-                    result += remainder;
-                }
-            }
-            return result;
-        }
-        
-        return text;
+        return result +"\n"+ wordWrap(remainder, columnWidth);
     }
 
-    private String wrapText(String text, int columnWidth, int currentPosition) {
-        String result="";
-        result = text.substring(currentPosition, currentPosition + columnWidth);
-        int whiteSpaceIndex = result.lastIndexOf(" ");
-        if (whiteSpaceIndex != -1) {
-            //result = result.replace(" ", "\n");
-            result=result.substring(0, whiteSpaceIndex-1)+"\n"+text.substring(whiteSpaceIndex);
-        } else {
-            result += "\n";
-        }
+    private String changeWhiteSpaceToLineWrap(String result, int lastWhiteSpaceIndex) {
+        StringBuilder textWithWhiteSpace = new StringBuilder(result);
+        textWithWhiteSpace.setCharAt(lastWhiteSpaceIndex, '\n');
+        result=textWithWhiteSpace.toString();
         return result;
-    }*/
-    //-----------------------------------------------------------------------------------------------
+    }
 }
