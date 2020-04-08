431d7c570702ee2a75ed578d6d2b07b8172795a5
Raúl Padilla Delgado <rpd27@outlook.com>
2020-04-08 12:02:34 +0100

    refactor 3

--- src/test/java/WordWrapShould.java
+++ src/test/java/WordWrapShould.java
@@ -13,65 +13,62 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WordWrapShould {
     @Test
     public void result_must_be_empty() {
-        assertThat(wordresultado("", 2)).isEqualTo("");
-        assertThat(wordresultado(null, 2)).isEqualTo("");
+        assertThat(wordWrap("", 2)).isEqualTo("");
+        assertThat(wordWrap(null, 2)).isEqualTo("");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void result_must_be_Ilegal_Argument_Exception() {
-        wordresultado("hola", 0);
+        wordWrap("hola", 0);
     }
 
     @Test
     public void text_must_be_wrap() {
-        assertThat(wordresultado("hola", 2)).isEqualTo("ho\nla");
-        assertThat(wordresultado("hola", 3)).isEqualTo("hol\na");
-        assertThat(wordresultado("hola mundo", 6)).isEqualTo("hola\nmundo");
-        assertThat(wordresultado("hola mundodffdfdfdfdf", 6)).isEqualTo("hola\nmundodf\nfdfdfd\nfdf");
-        assertThat(wordresultado("holaquetalestas", 2)).isEqualTo("ho\nla\nqu\net\nal\nes\nta\ns");
-        assertThat(wordresultado("holaquetalestas", 3)).isEqualTo("hol\naqu\neta\nles\ntas");
+        assertThat(wordWrap("hola", 2)).isEqualTo("ho\nla");
+        assertThat(wordWrap("hola", 3)).isEqualTo("hol\na");
+        assertThat(wordWrap("hola mundo", 6)).isEqualTo("hola\nmundo");
+        assertThat(wordWrap("hola mundodffdfdfdfdf", 6)).isEqualTo("hola\nmundodf\nfdfdfd\nfdf");
+        assertThat(wordWrap("holaquetalestas", 2)).isEqualTo("ho\nla\nqu\net\nal\nes\nta\ns");
+        assertThat(wordWrap("holaquetalestas", 3)).isEqualTo("hol\naqu\neta\nles\ntas");
     }
 
-    private String wordresultado(String texto, int anchoColumna) {
-        if (anchoColumna < 1) {
+    private String wordWrap(String text, int columnWidth) {
+        if (columnWidth < 1) {
             throw new IllegalArgumentException("Column width must be one or more");
         }
-        if (texto == null) {
+        if (text == null) {
             return "";
         }
 
-        boolean textoMayorQueAnchoColumna = texto.length() > anchoColumna;
-        if (textoMayorQueAnchoColumna) {
-            String ajuste = "";
-            for (int currentPosition = 0; currentPosition < texto.length(); currentPosition += anchoColumna) {
-                boolean textoSePuedeAjustar = currentPosition < texto.length() - anchoColumna;
-                if (textoSePuedeAjustar) {
-                    ajuste += ajustarTexto(texto, anchoColumna, currentPosition);
+        boolean textIsGreaterThanColumnWidth = text.length() > columnWidth;
+        if (textIsGreaterThanColumnWidth) {
+            String result = "";
+            for (int currentPosition = 0; currentPosition < text.length(); currentPosition += columnWidth) {
+                boolean currentPositionIsLessThanRemainderText = currentPosition < text.length() - columnWidth;
+                if (currentPositionIsLessThanRemainderText) {
+                    result += wrapText(text, columnWidth, currentPosition);
                 } else {
-                    ajuste += agregarResto(texto, currentPosition);
+                    String remainder = text.substring(currentPosition); 
+                    result += remainder;
                 }
             }
-            return ajuste;
+            return result;
         }
         
-        return texto;
+        return text;
     }
 
-    private String agregarResto(String texto, int currentPosition) {
-        String ajuste;
-        ajuste = texto.substring(currentPosition);
-        return ajuste;
-    }
+    
 
-    private String ajustarTexto(String texto, int anchoColumna, int currentPosition) {
-        String ajuste;
-        ajuste = texto.substring(currentPosition, currentPosition + anchoColumna);
-        int whiteSpaceIndex = ajuste.indexOf(" ");
+    private String wrapText(String text, int columnWidth, int currentPosition) {
+        String result;
+        result = text.substring(currentPosition, currentPosition + columnWidth);
+        int whiteSpaceIndex = result.indexOf(" ");
         if (whiteSpaceIndex != -1) {
-            ajuste = ajuste.replace(" ", "\n");
+            result = result.replace(" ", "\n");
         } else {
-            ajuste += "\n";
+            result += "\n";
         }
-        return ajuste;
+        return result;
     }
 }
